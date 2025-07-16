84534e5ac8e932d7c3dae1cd2d8835b2afe36cef
MarwaKHTanani <tananimarwa@gmail.com>
2025-07-16 15:35:53 +0300

    adding all section and styling it Relates #6

--- data.js
+++ data.js
@@ -23,3 +23,66 @@ const topNews = [
     urlToImage: "https://biztoc.com/cdn/950/og.png",
   },
 ];
+const newsData = {
+  tech: [
+    {
+      id: 1,
+      title:
+        "Bye-bye bots: Altera's game-playing AI agents get backing from Eric Schmidt | TechCrunch",
+      description:
+        "Autonomous, AI-based players are coming to a gaming experience near you, and a new startup, Altera, is joining the fray to build this new guard of AI Research company Altera raised $9 million to build AI agents that can play video games alongside other player…",
+      urlToImage:
+        "https://techcrunch.com/wp-content/uploads/2024/05/Minecraft-keyart.jpg?resize=1200,720",
+    },
+    {
+      id: 2,
+      title:
+        "$450M for Noname, two billion-dollar rounds, and good news for crypto startups | TechCrunch",
+      description:
+        "This morning on Equity, not only do we have good news for crypto founders, we're also digging into Akamai spending $450 million for API security firm Noname, and billion dollar deals from Wiz and Wayve.",
+      urlToImage:
+        "https://techcrunch.com/wp-content/uploads/2023/07/GettyImages-942480316.jpg?resize=1200,835",
+    },
+  ],
+  politics: [
+    {
+      id: 1,
+      title:
+        "Stock futures slip as investors await more bank earnings and inflation data: Live updates - CNBC",
+      description:
+        "The Nasdaq Composite climbed to a record close on Tuesday, aided by gains in Nvidia.",
+      urlToImage:
+        "https://image.cnbcfm.com/api/v1/image/108125336-1743627684990-gettyimages-2208186101-anotherday9612409_9piwixmy.jpeg?v=1743627816&w=1920&h=1080",
+    },
+    {
+      id: 2,
+      title:
+        "Trump Says Most House GOP Holdouts Have Agreed To Back His Crypto Bills—What To Know - Forbes",
+      description:
+        "Thirteen Republicans, mostly hardline conservatives, voted alongside all Democrats to block a procedural vote on the crypto legislative package.",
+      urlToImage:
+        "https://imageio.forbes.com/specials-images/imageserve/6876c692c7f8bf9f3429d784/0x0.jpg?format=jpg&height=900&width=1600&fit=bounds",
+    },
+  ],
+  sports: [],
+  economy: [],
+};
+
+const topBlogs = [
+  {
+    id: 1,
+    title: "6 Best Android Tablets (2025), Tested and Reviewed",
+    description:
+      "Forget the iPad. Catch up on your favorite shows in bed with the best Android tablets, from the OnePlus Pad 3 to Samsung's Galaxy Tab S10 FE.",
+    urlToImage:
+      "https://media.wired.com/photos/6875b2afafd81faa7ee2c914/191:100/w_2580,c_limit/The%20Best%20Android%20Tablets.png",
+  },
+  {
+    id: 2,
+    title: "Our biggest questions about ChromeOS and Android merging",
+    description:
+      "Samit Sarkar, Google's head of Android ecosystems, recently confirmed that Android and ChromeOS will be merged into a single platform, reigniting a slew of questions about the future of the operating systems. Combining them makes sense. It made sense way back…",
+    urlToImage:
+      "https://platform.theverge.com/wp-content/uploads/sites/2/2025/07/257814_Lenovo_Chromebook_Plus_14_laptop_review_ADiBenedetto_0020.jpg?quality=90&strip=all&crop=0%2C10.732984293194%2C100%2C78.534031413613&w=1200",
+  },
+];
--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 window.addEventListener("DOMContentLoaded", () => {
-const prev=document.querySelector('.prev')
-const next=document.querySelector('.next')
-const slider= document.getElementById('slider')
-let current=0;
+  const prev = document.querySelector(".prev");
+  const next = document.querySelector(".next");
+  const slider = document.getElementById("slider");
+  let current = 0;
 
-const showNews=(index)=>{
+  const showNews = (index) => {
     const news = topNews[index];
     slider.innerHTML = `
     <img src="${news.urlToImage}" alt="${news.title}" class="w-full h-64 object-cover rounded-t-lg"/>
@@ -13,16 +13,51 @@ const showNews=(index)=>{
       <p class="text-gray-700 text-sm">${news.description}</p>
     </div>
   `;
-}
-prev.addEventListener('click',()=>{
-    current= current===0? topNews.length-1: current-1
-    showNews(current)
+  };
+  const categoryrender = (category, categoryclass) => {
+    const classes = document.querySelector(`.${categoryclass}`);
+    classes.innerHTML = "";
+    newsData[category].forEach((news) => {
+      const card = document.createElement("div");
+      card.className = "news-card";
 
-})
-next.addEventListener('click',()=>{
-    current= current===topNews.length-1?0: current+1
-    showNews(current)
+      card.innerHTML = `
+        <img src="${news.urlToImage}" alt="${news.title}">
+        <h4>${news.title}</h4>
+        <p>${news.description}</p>
+        `;
+        classes.appendChild(card)
+    });
+  };
+  categoryrender('politics','politics-news')
+  categoryrender('tech','tech-news')
 
-})
-showNews(current)
-})
+
+  const blogrender = (blogs, blogclass) => {
+    const classes = document.querySelector(`.${blogclass}`);
+    classes.innerHTML = "";
+    blogs.forEach(blog => {
+      const card = document.createElement("div");
+      card.className = "news-card";
+
+      card.innerHTML = `
+        <img src="${blog.urlToImage}" alt="${blog.title}">
+        <h4>${blog.title}</h4>
+        <p>${blog.description}</p>
+        `;
+        classes.appendChild(card)
+    });
+  };
+  blogrender(topBlogs,'blog-news')
+
+
+  prev.addEventListener("click", () => {
+    current = current === 0 ? topNews.length - 1 : current - 1;
+    showNews(current);
+  });
+  next.addEventListener("click", () => {
+    current = current === topNews.length - 1 ? 0 : current + 1;
+    showNews(current);
+  });
+  showNews(current);
+});
